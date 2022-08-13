f4f29c39c8b2607dccc066fb01d34261842e9024
Mike Jerome <m@j4e.name>
2022-08-13 15:49:25 +0100

    Switch to getSignedUrlPromise

--- pages/api/bucket.js
+++ pages/api/bucket.js
@@ -1,11 +1,10 @@
 const AWS = require("aws-sdk");
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const s3 = new AWS.S3();
   const params = {
     Bucket: "let-me-see-if-i-can-upload",
     Key: "i-am-bucket",
   };
-  s3.getSignedUrl("putObject", params, function (err, url) {
-    return res.status(200).json({ url: url });
-  });
+  let url = await s3.getSignedUrlPromise("putObject", params);
+  return res.status(200).json({ url: url });
 }
