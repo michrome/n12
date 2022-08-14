0f792f92cc8159811fb02686a3fcb5ca0a3f33d3
Mike Jerome <m@j4e.name>
2022-08-14 16:51:45 +0100

    Read ENVs

--- pages/api/bucket.js
+++ pages/api/bucket.js
@@ -1,6 +1,9 @@
 const AWS = require("aws-sdk");
 export default async function handler(req, res) {
-  const s3 = new AWS.S3();
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  });
   const params = {
     Bucket: "let-me-see-if-i-can-upload",
     Key: "i-am-bucket",
